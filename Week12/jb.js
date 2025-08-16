0600704c2dc83aafc4830ef7b7972c5c457bf1a5
pragesh10 <basnetpragesh@gmail.com>
2025-08-16 19:29:19 +0545

    Add files via upload

---
+++ Week12/jb.js
@@ -0,0 +1,15 @@
+        function color1() {
+            document.getElementById("demo").innerHTML = "This is content 1";
+            document.getElementById("demo").style.backgroundColor = "black";
+            document.getElementById("demo").style.color = "white"; 
+        }
+        function color2() {
+            document.getElementById("demo").innerHTML = "This is content 2";
+            document.getElementById("demo").style.backgroundColor = "red";
+            document.getElementById("demo").style.color = "white";
+        }
+        function color3() {
+            document.getElementById("demo").innerHTML = "This is content 3";
+            document.getElementById("demo").style.backgroundColor = "purple";
+            document.getElementById("demo").style.color = "white";
+        }
